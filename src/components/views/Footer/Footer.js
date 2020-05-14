61d6da38901bd0c1b9fa8e7be7a3917af6bf84bd
1trantienan <s3699000@rmit.edu.vn>
2020-05-14 22:37:25 +0700

    Upload Front-end

---
+++ src/components/views/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Icon} from 'antd';
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p> Copyright © 2020 all rights reserved <Icon type="smile" /></p>
+        </div>
+    )
+}
+
+export default Footer
